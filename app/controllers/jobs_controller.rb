6401ec31d5bbafa97a4125e631890db3c6e24d8c
Sharon Cheung <keikei1128@g.ucla.edu>
2020-05-05 22:23:29 -0700

    update actions

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,5 +3,53 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+  
+  def show
+    @job = Job.find(params[:id])
+    if @job.nil?
+      head :not_found
+    end
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(
+      title: params[:job][:title],
+      description: params[:job][:description],
+      company: params[:job][:company]
+    )
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new, :bad_request
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    # find that job that we want to update
+    @job = Job.find(params[:id])
+    # update the job
+    if @job.update(
+      title: params[:job][:title],
+      description: params[:job][:description],
+      company: params[:job][:company]
+    )
+    # redirect if job is successfully update
+      redirect_to job_path(@job.id)
+    else
+      # if not stay in the edit form
+      render :edit, :bad_request
+    end
+  end
+
+  def delete
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/jobs/new', to: 'jobs#new'
+  post '/jobs', to: 'jobs#create'
+  
   get '/jobs', to: "jobs#index"
+  get "/jobs/:id", to: "jobs#show", as: "job"
+  get '/jobs/:id/edit', to: 'jobs#edit', as: 'edit_job'
+  patch '/jobs/:id',to: 'jobs#update'
 end
